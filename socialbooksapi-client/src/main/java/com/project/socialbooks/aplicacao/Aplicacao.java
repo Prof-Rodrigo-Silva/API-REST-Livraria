f9a8c854643547e67fbd88423c8afe911a41336b
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2019-08-21 17:06:35 -0300

    Add index and JQuery

--- socialbooksapi-client/src/main/java/com/project/socialbooks/aplicacao/Aplicacao.java
+++ socialbooksapi-client/src/main/java/com/project/socialbooks/aplicacao/Aplicacao.java
@@ -11,7 +11,7 @@ public class Aplicacao {
 	
 	public static void main(String[] args) throws ParseException, Exception{
 
-		LivrosClient cliente = new LivrosClient();
+		LivrosClient cliente = new LivrosClient("http://localhost:8080","rodrigo","s3nh4");
 		
 		List<Livro> listaLivros = cliente.listar();
 	
@@ -21,17 +21,23 @@ public class Aplicacao {
 			System.out.println("Livro: "+livro.getNome());
 		}
 		Livro livro = new Livro();
-		livro.setNome("Android");
+		livro.setNome("JQuery");
 		livro.setEditora("LEB");
 		
 		SimpleDateFormat publicacao = new SimpleDateFormat("dd/MM/yyyy");
 		livro.setPublicacao(publicacao.parse("21/08/2019"));
 		
-		livro.setResumo("PHP Avançado");
+		livro.setResumo("JQuery simples para você");
 		
 		String localizacao = cliente.salvar(livro);
 		
 		System.out.println("URI do livro salvo: "+ localizacao);
+		
+		Livro livroBuscado = cliente.buscar(localizacao);
+		
+		System.out.println("Livro buscado: "+ livroBuscado.getNome());
+		
+		
 		
 	}
 
